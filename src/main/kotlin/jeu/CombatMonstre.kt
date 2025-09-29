aa0d4b3b33e0d49ac312c30e373dc8ffba4c8be5
issx77 <issa7791100@gmail.com>
2025-09-29 12:00:39 +0200

    combat

--- src/main/kotlin/jeu/CombatMonstre.kt
+++ src/main/kotlin/jeu/CombatMonstre.kt
@@ -1,12 +1,13 @@
 package org.example.jeu
-
+import org.example.item.Item
 import org.example.dresseur.Entraineur
+import org.example.item.Utilisable
 import org.example.joueur
 import org.example.monstre.IndividuMonstre
 
 class CombatMonstre(
     val joueur: Entraineur,
-    val monstreJoueur: IndividuMonstre,
+    var monstreJoueur: IndividuMonstre,
     val monstreSauvage: IndividuMonstre
 ){
     var round: Int = 1
@@ -22,6 +23,65 @@ fun gameOver(): Boolean{
 
 fun joueurGagne(): Boolean{
 
-    if ( monstreSauvage.pv<=0 )
+    if ( monstreSauvage.pv<=0 ){
+        println("${joueur.nom} a gagné!!")
+        val gainExp= monstreSauvage.exp*0.20
+        monstreJoueur.exp=monstreJoueur.exp+gainExp
+        println("${monstreJoueur.nom} gagne ${gainExp} exp")
+        return true
+    }
+    else{
+        if (monstreSauvage.entraineur==joueur){
+            println("${monstreSauvage.nom} a été capturé !")
+            return true
+
+        }
+        else{return false}
+
+    }
+}
+fun actionAdversaire(){
+    if (monstreSauvage.pv>0){
+        monstreSauvage.attaquer(monstreJoueur)
+    }
+}
+fun actionJoueur(): Boolean{
+    if (gameOver()==true){
+        return false
+    }
+    else{
+        println("menu choix d'actions :" +
+                "/t 1 -> Attaquer /n" +
+                "/t 2 -> utiliser un objet/n" +
+                "/t 3 -> changer de monstre")
+        var choixAction = readln()
+        while ( choixAction.toInt() !in (1..3)){
+            println("Veuillez saisir une action valable")
+            choixAction = readln()
+        }
+        if (choixAction.toInt()==1){
+            monstreJoueur.attaquer(monstreSauvage)
+            return true
+        }
+        else if (choixAction.toInt()==2){
+            for ((index,objet) in this.joueur.sacItems.withIndex()){
+                println("$index => ${objet}")
+            }
+            var choixObjet = readln()
+            while ( choixObjet.toInt() !in (0..joueur.sacItems.size-1)){
+                println("Veuillez saisir une action valable")
+                choixObjet = readln()
+            }
+            val obj= joueur.sacItems[choixObjet.toInt()]
+            if(obj is Utilisable){
+                val result= obj.utiliser(monstreSauvage)
+                if (result==true){
+                    return false
+                }
+            }else{println("Objet non utilisable")}
+
+        }
+
+    }
 }
 }
